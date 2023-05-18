e9598e081d1fef11ed74a7f971690ebd024c415d
dustisme <androsrivas@outlook.com>
2023-05-18 16:09:53 +0200

    update Action corrected

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -4,31 +4,38 @@
  * Base controller for the application.
  * Add general things in this controller.
  */
-class ApplicationController extends Controller 
+class ApplicationController extends Controller
 {
-	private $persistence;
-    public function __construct() {
+    private $persistence;
+    public function __construct()
+    {
         $this->persistence = new Persistence();
     }
-    public function indexAction() {
+    public function indexAction()
+    {
         $this->view->listTasks = $this->persistence->listTasks();
     }
-    public function viewTaskAction() {
+    public function viewTaskAction()
+    {
         $this->view->viewTask = $this->persistence->viewTask($this->_namedParameters['id']);
     }
-    public function goToUdateTaskAction() {
+    public function editTaskAction()
+    {
         $this->view->viewTask = $this->persistence->viewTask($this->_namedParameters['id']);
     }
-    public function updateTask() {
-        $this->persistence->updateTask($this->_namedParameters['id']);
-        header ("Location: " . WEB_ROOT . "/");
+    public function updateTaskAction()
+    {
+        $this->persistence->updateTask($this->_namedParameters['id'], $_POST['username'], $_POST['taskDescription'], $_POST['status'], $_POST['startingDate'], $_POST['finishedDate']);
+        header("Location: " . WEB_ROOT . "/");
     }
-    public function addTaskAction() {
+    public function addTaskAction()
+    {
         $this->persistence->addTask();
-        header ("Location: " . WEB_ROOT . "/");
+        header("Location: " . WEB_ROOT . "/");
     }
-    public function deleteTaskAction() {
+    public function deleteTaskAction()
+    {
         $this->persistence->deleteTask($_POST['id']);
-        header ("Location: " . WEB_ROOT . "/");
+        header("Location: " . WEB_ROOT . "/");
     }
 }
