a82a4aa192e27c7c8069547dd647d993f8fac9b6
dustisme <androsrivas@outlook.com>
2023-05-19 10:40:04 +0200

    interface correcetd

--- app/models/persistenceInterface.php
+++ app/models/persistenceInterface.php
@@ -5,7 +5,7 @@ interface PersistenceInterface {
     public function listTasks();
     public function viewTask($task_id);
     public function updateTask($task_id, Array $data);
-    public function addTask();
+    public function addTask(Array $data);
     public function deleteTask($task_id);
     public function searchTask($task_id);
     public function addDataToJson($task_array);
