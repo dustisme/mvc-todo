8796bba7ee806e7bd0243c6068ccdad6957c1df1
dustisme <androsrivas@outlook.com>
2023-05-19 10:39:49 +0200

    task properties redefined

--- app/models/Task.php
+++ app/models/Task.php
@@ -1,56 +1,56 @@
 <?php
-include_once 'Status.php';
-
     class Task {
-        protected int $task_id;
-        protected string $task_description;
+        protected int $taskId;
+        protected string $taskDescription;
         protected string $username;
-        protected $starting_date;
-        protected $finished_date;
-        private Status $status;
+        protected $startingDate;
+        protected $finishedDate;
+        private string $status;
 
         //Status is a constant var
-        function __construct(int $task_id, string $task_description, string $username, $starting_date, Status $status = Status::executing) {
-            $this->task_id = $task_id;
-            $this->task_description = $task_description;
+        function __construct($username, $taskDescription, $status, $startingDate) {
             $this->username = $username;
-            $this->starting_date = $starting_date;
+            $this->taskDescription = $taskDescription;
             $this->status = $status;
+            $this->startingDate = $startingDate;
         }
         //getters
         function getId() {
-            return $this->task_id;
+            return $this->taskId;
         }
-        function getTask_description() {
-            return $this->task_description;
+        function getTaskDescription() {
+            return $this->taskDescription;
         }
         function getUsername() {
             return $this->username;
         }
-        function getStarting_date() {
-            return $this->starting_date;
+        function getStartingDate() {
+            return $this->startingDate;
         }
-        function getFinished_date() {
-            return $this->finished_date;
+        function getFinishedDate() {
+            return $this->finishedDate;
         }
         function getStatus() {
             return $this->status;
         }
 
         //setters
-        function setTask_description(string $task) {
-            $this->task_description = $task;
+        function setId(int $taskId) {
+            $this->taskId = $taskId;
+        }
+        function setTaskDescription(string $taskDescription) {
+            $this->taskDescription = $taskDescription;
         }
         function setUsername(string $username) {
             $this->username = $username;
         }
-        function setStarting_date(string $starting_date) {
-        $this->starting_date = $starting_date;
+        function setStartingDate($startingDate) {
+        $this->startingDate = $startingDate;
         }
-        function setFinished_date(string $finished_date) {
-            $this->finished_date = $finished_date;
+        function setFinishedDate($finishedDate) {
+            $this->finishedDate = $finishedDate;
         }
-        function setStatus(Status $status) {
+        function setStatus(string $status) {
             $this->status = $status;
         }
     }
